d089d8bb29968a869d2c59abd3d77f88860f1857
Cameron <cameron.robertson2201@gmail.com>
2025-12-22 22:21:15 +0000

    v0.1.2 release

--- Formula/opentui-git.rb
+++ Formula/opentui-git.rb
@@ -6,12 +6,12 @@ class OpentuiGit < Formula
 
   on_arm do
     url "https://github.com/crob19/opentui-git/releases/download/v0.1.2/opentui-git-v0.1.2-darwin-arm64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "73e1e07b90def2cd6ecc40e4a840b960e15252d407fee9c5bc224dccd4c60bb2"
   end
 
   on_intel do
     url "https://github.com/crob19/opentui-git/releases/download/v0.1.2/opentui-git-v0.1.2-darwin-x64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "75df46cb63774e4ee1724cabfda648ffaf7d8318ecfd99c4bdf2915cc08f093b"
   end
 
   def install
